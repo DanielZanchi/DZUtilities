1b35c683f1184633eb18a79166fdb724b04f2766
Daniel Zanchi <dev@danielzanchi.com>
2021-11-23 18:24:07 +0100

    string random

---
+++ Sources/DZUtilities/String.Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Daniel Zanchi on 23/11/21.
+//
+
+import Foundation
+
+extension String {
+    
+    func randomString(length: Int) -> String {
+      let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+      return String((0..<length).map{ _ in letters.randomElement()! })
+    }
+    
+}
