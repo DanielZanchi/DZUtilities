82fd3c5a76434273a4cf2e828e299cc85a68396c
Daniel Zanchi <dev@danielzanchi.com>
2022-02-03 19:11:44 +0100

    windows scene

--- Sources/DZUtilities/Extensions/UIApplication.Extensions.swift
+++ Sources/DZUtilities/Extensions/UIApplication.Extensions.swift
@@ -18,3 +18,14 @@ extension UIApplication {
     }
     
 }
+
+extension UIApplication {
+    
+    public var currentWindowScene: UIWindowScene? {
+        return UIApplication.shared.connectedScenes
+            .filter { $0.activationState == .foregroundActive }
+            .first(where: { $0 is UIWindowScene })
+            .flatMap({ $0 as? UIWindowScene })
+    }
+    
+}
