e36e1f6d92186ae1df40eadceab30584fb8ff66f
Daniel Zanchi <dev@danielzanchi.com>
2022-02-08 18:54:13 +0100

    loadFonts

--- Sources/DZUtilities/DZUtilities.swift
+++ Sources/DZUtilities/DZUtilities.swift
@@ -1,5 +1,44 @@
+import UIKit
+
+public let DZUtils = DZUtilities.shared
+
 public struct DZUtilities {
 
-    public init() {
+    public static let shared = DZUtilities()
+
+    private init() {
     }
+    
+    public func loadFonts() {
+        func getFontsURLs() -> [URL]? {
+            if let resourceURL = Bundle.main.resourceURL {
+                let fontsURL = resourceURL.appendingPathComponent("Fonts")
+                guard var URLs = try? FileManager.default.contentsOfDirectory(at: fontsURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles) else {
+                    return nil
+                }
+                URLs = URLs.filter { $0.absoluteString.contains(".ttf") }
+                return URLs
+            }
+            return nil
+        }
+
+        let fonts = getFontsURLs()
+        fonts?.forEach{ url in
+            do {
+                let inData: NSData = try NSData(contentsOfFile: url.path)
+                let error: UnsafeMutablePointer<Unmanaged<CFError>?>? = nil
+                let provider = CGDataProvider.init(data:inData)
+                if let font = CGFont.init(provider!) {
+                    if (!CTFontManagerRegisterGraphicsFont(font, error)) {
+                        print(error.debugDescription)
+                    }
+                }
+                CTFontManagerRegisterFontsForURL(url as CFURL, .process, nil)
+                print("Loaded : \(url.lastPathComponent)")
+            } catch {
+                print("error \(url) - \(error.localizedDescription)")
+            }
+        }
+    }
+    
 }
