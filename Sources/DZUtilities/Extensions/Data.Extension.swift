682ec3cf5fa000b17a1a31afaa270aa276c199dd
Daniel Zanchi <danny.zanchi@gmail.com>
2022-05-20 13:12:07 +0200

    removed double

--- Sources/DZUtilities/Extensions/Data.Extension.swift
+++ Sources/DZUtilities/Extensions/Data.Extension.swift
@@ -9,17 +9,6 @@ import Foundation
 
 extension Data {
     
-    public func getSizeInMB() -> Double {
-        let bcf = ByteCountFormatter()
-        bcf.allowedUnits = [.useMB]
-        bcf.countStyle = .file
-        let string = bcf.string(fromByteCount: Int64(self.count)).replacingOccurrences(of: ",", with: ".")
-        if let double = Double(string.replacingOccurrences(of: " MB", with: "")) {
-            return double
-        }
-        return 0.0
-    }
-    
     public func getSizeInMB() -> String {
         let bcf = ByteCountFormatter()
         bcf.allowedUnits = [.useMB]
