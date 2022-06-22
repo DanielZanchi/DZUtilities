4109adedbcf88aeb87fef968451433399400e16d
Daniel Zanchi <danny.zanchi@gmail.com>
2022-06-22 12:05:35 +0200

    iOS 13 widnows

--- Sources/DZUtilities/Extensions/UIApplication.Extensions.swift
+++ Sources/DZUtilities/Extensions/UIApplication.Extensions.swift
@@ -7,27 +7,33 @@
 
 import UIKit
 
-@available(iOS 14, *)
 extension UIApplication {
     
     public var getKeywindow: UIWindow? {
-        return UIApplication.shared.connectedScenes
-            .filter { $0.activationState == .foregroundActive }
-            .first(where: { $0 is UIWindowScene })
-            .flatMap({ $0 as? UIWindowScene })?.windows
-            .first(where: \.isKeyWindow)
+        if #available(iOS 13.0, *) {
+            return UIApplication.shared.connectedScenes
+                .filter { $0.activationState == .foregroundActive }
+                .first(where: { $0 is UIWindowScene })
+                .flatMap({ $0 as? UIWindowScene })?.windows
+                .first(where: \.isKeyWindow)
+        } else {
+            return UIApplication.shared.keyWindow
+        }
     }
     
 }
 
-@available(iOS 14, *)
 extension UIApplication {
     
     public var currentWindowScene: UIWindowScene? {
-        return UIApplication.shared.connectedScenes
-            .filter { $0.activationState == .foregroundActive }
-            .first(where: { $0 is UIWindowScene })
-            .flatMap({ $0 as? UIWindowScene })
+        if #available(iOS 13.0, *) {
+            return UIApplication.shared.connectedScenes
+                .filter { $0.activationState == .foregroundActive }
+                .first(where: { $0 is UIWindowScene })
+                .flatMap({ $0 as? UIWindowScene })
+        } else {
+            return UIApplication.shared.currentWindowScene
+        }
     }
     
 }
