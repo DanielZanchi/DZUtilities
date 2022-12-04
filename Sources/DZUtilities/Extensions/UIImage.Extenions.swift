5fdc460662818de7cf26894c0be8daa95768f3c5
Daniel Zanchi <danny.zanchi@gmail.com>
2022-12-04 10:48:26 +0100

    new methods to resize images

--- Sources/DZUtilities/Extensions/UIImage.Extenions.swift
+++ Sources/DZUtilities/Extensions/UIImage.Extenions.swift
@@ -9,17 +9,20 @@ import UIKit
 
 extension UIImage {
     
-    public func resize(to newSize: CGSize) -> UIImage {
-        let renderer = UIGraphicsImageRenderer(size: newSize)
-        let image = renderer.image { _ in
+    public func resize(to newSize: CGSize, scale: CGFloat? = nil, quality: CGInterpolationQuality = .default) -> UIImage {
+        let format = UIGraphicsImageRendererFormat.default()
+        format.scale = scale ?? self.scale
+        let renderer = UIGraphicsImageRenderer(size: newSize, format: format)
+        let image = renderer.image { context in
+            context.cgContext.interpolationQuality = quality
             self.draw(in: CGRect.init(origin: CGPoint.zero, size: newSize))
         }
         return image
     }
     
-    public func resizedImage(withLongestLenghtOf longest: CGFloat) -> UIImage {
+    public func getNewSize(withLongestLenghtOf longest: CGFloat) -> CGSize {
         if longest > self.size.width && longest > self.size.height {
-            return self
+            return self.size
         }
         
         var factor: CGFloat
@@ -33,15 +36,14 @@ extension UIImage {
         let newHeight = factor * self.size.height
         
         let newSize = CGSize(width: Int(newWidth), height: Int(newHeight))
+        return newSize
+    }
+    
+    public func resizedImage(withLongestLenghtOf longest: CGFloat, scale: CGFloat? = nil, quality: CGInterpolationQuality = .default) -> UIImage {
+
+        let newSize = getNewSize(withLongestLenghtOf: longest)
         
-        let format = UIGraphicsImageRendererFormat.default()
-        format.scale = scale
-        let renderer = UIGraphicsImageRenderer(size: newSize, format: format)
-        let image = renderer.image { _ in
-            draw(in: CGRect(origin: .zero, size: newSize))
-        }
-        
-        return image
+        return resize(to: newSize, scale: scale, quality: quality)
     }
     
 }
