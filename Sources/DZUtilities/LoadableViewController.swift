b6c23f482d561033d2c57b96f53ac0763d36cd5c
Daniel Zanchi <daniel.zanchi@spacemushrooms.it>
2023-01-16 08:49:08 +0100

    LoadableViewController

---
+++ Sources/DZUtilities/LoadableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Daniel Zanchi on 16/01/23.
+//
+
+import UIKit
+
+class LoadableViewController: UIViewController {
+	
+	static func nibName() -> String {
+		return String(describing: self)
+	}
+	
+	init() {
+		super.init(nibName: Self.nibName(), bundle: nil)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+}
