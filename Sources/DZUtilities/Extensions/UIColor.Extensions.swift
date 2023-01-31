6190bde6ac44fa5f9611c6827d37cb5abd1852a3
Daniel Zanchi <daniel.zanchi@spacemushrooms.it>
2023-01-31 16:16:23 +0100

    hex colors

--- Sources/DZUtilities/Extensions/UIColor.Extensions.swift
+++ Sources/DZUtilities/Extensions/UIColor.Extensions.swift
@@ -9,15 +9,61 @@ import UIKit
 import SwiftUI
 
 extension UIColor {
-    // Construct a color from HEXRGB
-    public convenience init(rgbHEX: UInt) {
-        self.init(
-            red: CGFloat((rgbHEX & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbHEX & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbHEX & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
-    }
+	
+	var hexString: String {
+		let cgColorInRGB = cgColor.converted(to: CGColorSpace(name: CGColorSpace.sRGB)!, intent: .defaultIntent, options: nil)!
+		let colorRef = cgColorInRGB.components
+		let r = colorRef?[0] ?? 0
+		let g = colorRef?[1] ?? 0
+		let b = ((colorRef?.count ?? 0) > 2 ? colorRef?[2] : g) ?? 0
+		let a = cgColor.alpha
+
+		var color = String(
+			format: "#%02lX%02lX%02lX",
+			lroundf(Float(r * 255)),
+			lroundf(Float(g * 255)),
+			lroundf(Float(b * 255))
+		)
+
+		if a < 1 {
+			color += String(format: "%02lX", lroundf(Float(a * 255)))
+		}
+
+		return color
+	}
+	
+	// Construct a color from HEXRGB
+	public convenience init?(hex: String) {
+		let r, g, b, a: CGFloat
+		
+		if hex.hasPrefix("#") {
+			let start = hex.index(hex.startIndex, offsetBy: 1)
+			let hexColor = String(hex[start...])
+			
+			let scanner = Scanner(string: hexColor)
+			var hexNumber: UInt64 = 0
+			
+			if scanner.scanHexInt64(&hexNumber) {
+				if hexColor.count == 8 {
+					r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+					g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+					b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+					a = CGFloat(hexNumber & 0x000000ff) / 255
+					
+					self.init(red: r, green: g, blue: b, alpha: a)
+					return
+				} else if hexColor.count == 6 {
+					r = CGFloat((hexNumber & 0xFF0000) >> 16) / 255.0
+					g = CGFloat((hexNumber & 0x00FF00) >> 8) / 255.0
+					b = CGFloat(hexNumber & 0x0000FF) / 255.0
+					self.init(red: r, green: g, blue: b, alpha: 1)
+					return
+				}
+			}
+		}
+		
+		return nil
+	}
     
     // Construct a color from rgb values 0-255
     public convenience init(r: Int, g: Int, b: Int, alpha: CGFloat) {
