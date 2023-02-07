f84fbab78592e6ddec5f2a329da37f3ed2f10711
Daniel Zanchi <danny.zanchi@gmail.com>
2023-02-07 19:58:31 +0100

    random string with only letters

--- Sources/DZUtilities/Extensions/String.Extensions.swift
+++ Sources/DZUtilities/Extensions/String.Extensions.swift
@@ -8,20 +8,33 @@
 import Foundation
 
 extension String {
-    
-    static public func randomString(length: Int) -> String {
-      let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-      return String((0..<length).map{ _ in letters.randomElement()! })
-    }
-    
+	
+	public enum RandomType { case full, onlyLetters, onlyLowercasedLetters, onlyUppercasedLetters }
+	
+	static public func randomString(length: Int, randomType: RandomType = .full) -> String {
+		var letters = ""
+		switch randomType {
+		case .full:
+			letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+		case .onlyLetters:
+			letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		case .onlyLowercasedLetters:
+			letters = "abcdefghijklmnopqrstuvwxyz"
+		case .onlyUppercasedLetters:
+			letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		}
+		
+		return String((0..<length).map{ _ in letters.randomElement()! })
+	}
+	
 }
 
 extension String {
-    public var localized: String {
-        return NSLocalizedString(self, comment: self)
-    }
-    
-    public func localized(variables: [CVarArg]) -> String {
-        return String(format: self.localized, arguments: variables)
-    }
+	public var localized: String {
+		return NSLocalizedString(self, comment: self)
+	}
+	
+	public func localized(variables: [CVarArg]) -> String {
+		return String(format: self.localized, arguments: variables)
+	}
 }
