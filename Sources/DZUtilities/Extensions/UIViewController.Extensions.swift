d3cbada3d5507a352e2109a8cb738846aca1e5e1
Daniel Zanchi <danny.zanchi@gmail.com>
2023-02-09 11:44:24 +0100

    addViewController

---
+++ Sources/DZUtilities/Extensions/UIViewController.Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Daniel Zanchi on 09/02/23.
+//
+
+import UIKit
+
+extension UIViewController {
+	
+	public func addViewController(_ viewController: UIViewController, inView view: UIView){
+		viewController.willMove(toParent: self)
+		viewController.view.frame = view.bounds
+		view.addSubview(viewController.view)
+		self.addChild(viewController)
+		viewController.didMove(toParent: self)
+	}
+	
+}
