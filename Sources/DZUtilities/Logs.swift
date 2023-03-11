99231cdff073087e3de0cb77ed1abc7b1f3ce89d
Daniel Zanchi <danny.zanchi@gmail.com>
2023-03-11 12:53:41 +0100

    removed warning

--- Sources/DZUtilities/Logs.swift
+++ Sources/DZUtilities/Logs.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public func DZError(_ text: String?, error: Error?, line: Int = #line, function: String = #function) {
 #if DEBUG
-	print("### 🔴 - \(text ?? ""): \(error?.localizedDescription ?? "") \n\n \(error) \n\n - in \(function) at \(line)")
+	print("### 🔴 - \(text ?? ""): \(error?.localizedDescription ?? "") \n\n \(String(describing: error)) \n\n - in \(function) at \(line)")
 #endif
 }
 
